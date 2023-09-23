acc5cb2daa7262ce6fc261787bb0d991ddcbc785
posheadig <57874590+posheadig@users.noreply.github.com>
2023-09-23 15:25:38 +0300

    Update mapping.ts

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,12 +1,19 @@
+import { SimpleToken } from '../generated/templates'
 import { TokenCreated } from "../generated/MintQuest/MintQuest"
 import { Token, TokenCreation } from "../generated/schema"
 
 export function handleTokenCreated(event: TokenCreated): void {
-    let token = new Token(event.params.tokenAddress.toHexString())
-    token.address = event.params.tokenAddress
-    token.owner = event.params.owner
-    token.name = event.params.name
-    token.symbol = event.params.symbol
+    let tokenCreated = new TokenCreated(event.transaction.hash.toHexString())
+    tokenCreated.tokenAddress = event.params.tokenAddress
+    tokenCreated.owner = event.params.owner
+
+    // Instantiate the token contract using the token's address
+    let tokenContract = SimpleToken.bind(event.params.tokenAddress)
+    
+    // Fetch the name and other details
+    tokenCreated.name = tokenContract.name()
+    tokenCreated.symbol = tokenContract.symbol()
+    
     token.totalSupply = 10000
     token.kind = event.params.kind
     token.content = event.params.content
