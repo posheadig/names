deb2b2d6d53b30d986870fb3366e2892c3670947
posheadig <57874590+posheadig@users.noreply.github.com>
2023-09-23 15:38:12 +0300

    Update mapping.ts

--- src/mapping.ts
+++ src/mapping.ts
@@ -3,21 +3,22 @@ import { TokenCreated } from "../generated/MintQuest/MintQuest"
 import { Token, TokenCreation } from "../generated/schema"
 
 export function handleTokenCreated(event: TokenCreated): void {
-    let tokenCreated = new TokenCreated(event.transaction.hash.toHexString())
-    tokenCreated.tokenAddress = event.params.tokenAddress
-    tokenCreated.owner = event.params.owner
-
+    let token = new Token(event.transaction.hash.toHexString())
+    token.address = event.params.tokenAddress
+    token.owner = event.params.owner
+    
     // Instantiate the token contract using the token's address
     let tokenContract = SimpleToken.bind(event.params.tokenAddress)
-    
+        
     // Fetch the name and other details
-    tokenCreated.name = tokenContract.name()
-    tokenCreated.symbol = tokenContract.symbol()
-    
-    token.totalSupply = 10000
-    token.kind = event.params.kind
-    token.content = event.params.content
+    token.name = tokenContract.name()
+    token.symbol = tokenContract.symbol()
+        
+    token.totalSupply = BigInt.fromI32(10000) // Convert the number to BigInt
+    token.kind = "Your kind value" // You might need to fetch or determine this
+    token.content = "Your content value" // You might need to fetch or determine this
     token.save()
+
 
     let creation = new TokenCreation(event.transaction.hash.toHexString())
     creation.event = "TokenCreated"
