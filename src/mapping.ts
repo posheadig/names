353f1b65c98cebd88c9a1ea33aee0cbae52eedd0
posheadig <57874590+posheadig@users.noreply.github.com>
2023-09-23 16:06:06 +0300

    Update mapping.ts

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,28 +1,17 @@
-import { SimpleToken } from '../generated/templates'
 import { TokenCreated } from "../generated/MintQuest/MintQuest"
-import { Token, TokenCreation } from "../generated/schema"
+import { SimpleToken as SimpleTokenTemplate } from "../generated/templates"
+import { Token } from "../generated/schema"
 
 export function handleTokenCreated(event: TokenCreated): void {
-    let token = new Token(event.transaction.hash.toHexString())
-    token.address = event.params.tokenAddress
-    token.owner = event.params.owner
-    SimpleToken.create(event.params.tokenAddress);
-    // Instantiate the token contract using the token's address
-    let tokenContract = SimpleToken.bind(event.params.tokenAddress)
-        
-    // Fetch the name and other details
-    token.name = tokenContract.name()
-    token.symbol = tokenContract.symbol()
-        
-    token.totalSupply = BigInt.fromI32(10000) // Convert the number to BigInt
-    token.kind = "Your kind value" // You might need to fetch or determine this
-    token.content = "Your content value" // You might need to fetch or determine this
-    token.save()
+    let tokenID = event.params.tokenAddress.toHexString();
+    let token = new Token(tokenID);
 
+    token.address = event.params.tokenAddress;
 
-    let creation = new TokenCreation(event.transaction.hash.toHexString())
-    creation.event = "TokenCreated"
-    creation.token = token.id
-    creation.timestamp = event.block.timestamp
-    creation.save()
+    // Fetching the token details
+    let tokenContract = SimpleTokenTemplate.bind(event.params.tokenAddress);
+    token.name = tokenContract.name();
+    token.symbol = tokenContract.symbol();
+
+    token.save();
 }
