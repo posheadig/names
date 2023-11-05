40e3efd11d904fda6a5a204ab58a5e93e5d23653
posheadig <57874590+posheadig@users.noreply.github.com>
2023-11-05 10:20:11 +0700

    Update mapping.ts

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,17 +1,28 @@
-import { TokenCreated } from "../generated/MintQuest/MintQuest"
-import { SimpleToken } from "../generated/SimpleToken/SimpleToken"
-import { Token } from "../generated/schema"
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  ovTokenBase as ovTokenBaseContract,
+  TokenCreated as TokenCreatedEvent,
+  PairCreated as PairCreatedEvent
+} from "../generated/ovTokenBase/ovTokenBase"
+import { Token, Pair } from "../generated/schema"
 
-export function handleTokenCreated(event: TokenCreated): void {
-    let tokenID = event.params.tokenAddress.toHexString();
-    let token = new Token(tokenID);
+export function handleTokenCreated(event: TokenCreatedEvent): void {
+  let token = new Token(event.params.token.toHex())
+  let contract = ovTokenBaseContract.bind(event.params.token)
+  let nameResult = contract.try_name() // This calls the 'name' function on your ERC20 contract
 
-    token.address = event.params.tokenAddress;
+  if (!nameResult.reverted) {
+    token.name = nameResult.value
+  }
+  token.address = event.params.token
+  token.save()
+}
 
-    // Instantiate the SimpleToken contract
-    let tokenContract = SimpleToken.bind(event.params.tokenAddress);
-    token.name = tokenContract.name();
-    token.symbol = tokenContract.symbol();
-
-    token.save();
+export function handlePairCreated(event: PairCreatedEvent): void {
+  let pair = new Pair(event.params.pair.toHex())
+  pair.tokenA = event.params.tokenA.toHex()
+  pair.tokenB = event.params.tokenB.toHex()
+  pair.address = event.params.pair
+  // ... other fields to populate
+  pair.save()
 }
